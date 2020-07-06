11b9debe72d46d4b9173241875bd7812d6d60ac0
Yan <rocyan98@gmail.com>
2020-07-06 15:57:53 +0800

    更新A1001

--- A1001.cpp
+++ A1001.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
 int main() {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    int sum = a + b;
-    if (sum < 0) {
-        sum = -sum;
-        printf("-");
-    }
-    string ans;
-    int flag = 0;
-    do {
-        if (flag && flag % 3 == 0) ans.push_back(',');  
-        ans.push_back(sum % 10 + '0');
-        sum /= 10;
-        flag++;
-    } while (sum);
-    reverse(ans.begin(), ans.end());
-    cout << ans;
-    return 0;
+	int a, b;
+	scanf("%d%d", &a, &b);
+	int sum = a + b;
+	if (sum < 0) {
+		sum = -sum;
+		printf("-");
+	}
+	string ans;
+	int flag = 0;
+	do {
+		if (flag && flag % 3 == 0) ans.push_back(',');	
+		ans.push_back(sum % 10 + '0');
+		sum /= 10;
+		flag++;
+	} while (sum);
+	reverse(ans.begin(), ans.end());
+	printf("%s", ans.c_str());
+	return 0;
 }
