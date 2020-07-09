3ebd019a1cbf11a0c1a16dbf69736d8e5ce99fb7
Yan <rocyan98@gmail.com>
2020-07-09 13:34:43 +0800

    long double 替换 long long

--- A1104.cpp
+++ A1104.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 
-#define LL long long
 using namespace std;
 
 int main() {
     int n;
     scanf("%d", &n);
-    LL sum = 0;
+    long double sum = 0;
     for (int i = 1; i <= n; i++) { 
-        double temp;
-        scanf("%lf", &temp);
-        sum += (LL)(temp * 1000) * i * (n - i + 1);
+        long double temp;
+        scanf("%llf", &temp);
+        sum += temp * i * (n - i + 1);
     }
-    printf("%.2f", sum / 1000.);
+    printf("%.2llf", sum);
     return 0;
 }
